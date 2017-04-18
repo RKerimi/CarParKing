e33cf6db76c23222f6ff2d1fdfa6636332cf2a45
RKerimi <rkerimi@gmail.com>
2017-04-18 18:13:32 +0200

    Datenbank einbinden

--- CarParKing/MainWindow.xaml.cs
+++ CarParKing/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace CarParKing
 {
@@ -23,6 +24,31 @@ namespace CarParKing
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+                "Data Source=DESKTOP-29EL2G5;" +
+                "Initial Catalog=CarParKingDB;" +
+                "Integrated Security=SSPI;";
+            conn.Open();
+            Console.WriteLine(conn.State);
+            using (SqlCommand command = new SqlCommand("SELECT * FROM parkplatzdetails", conn))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.WriteLine(reader.GetValue(i));
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
         }
     }
 }
--- CarParKing/ModelView/MainModelView.cs
+++ CarParKing/ModelView/MainModelView.cs
@@ -8,5 +8,7 @@ namespace CarParKing.ModelView
 {
     class MainModelView
     {
+
+
     }
 }
